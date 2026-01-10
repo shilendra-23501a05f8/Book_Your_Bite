3c32c406808f52413225931d7176d326370c79a7
shilendra-23501a05f8 <23501a05f8@pvpsit.ac.in>
2026-01-10 12:15:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import app from "./app.js";
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
-})
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`SERVER HAS STARTED AT PORT ${PORT}`);
+});
